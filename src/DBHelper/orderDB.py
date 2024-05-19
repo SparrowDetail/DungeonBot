f4d104eb823b1fa214d1a8ddc939b5048c771e6d
StevenS <sshort95@gmail.com>
2024-05-19 09:59:37 -0400

    Support added to update initiative without removing and adding

--- src/DBHelper/orderDB.py
+++ src/DBHelper/orderDB.py
@@ -113,3 +113,92 @@ def clear_user_order(user_id: int):
     conn.commit()
     conn.close()
     vacuum()
+
+def get_character_id(user_id: int, char_name: str) -> int:
+    """
+    Queries for the characters rowid within the SQLite database and returns the id or None if the character could
+    not be found.
+
+    :param user_id: FOREIGN KEY user_id, must exist within the users table
+    :param char_name: Character being verified
+    :return: Unique integer id for queried character or None if the character does not exist
+    """
+    conn = sqlite3.Connection(DATABASE_DIRECTORY)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+        SELECT rowid
+        FROM {ORDER_COMMAND_TABLE}
+        WHERE {USER_ID} = {user_id} AND {NAME} = '{char_name}';""")
+    
+    result: int = cursor.fetchone()
+
+    conn.commit()
+    conn.close()
+
+    return result[0] if (not result == None) else result
+
+def update_character_by_id(char_id: int, char_name:str = None, roll_value: int = None, modifier: int = None) -> bool:
+    """
+    Updates the character data of a particular row within the order table using a unique row ID.
+
+    :param char_id: SQLite generated row id for the character being updated
+    :param char_name: Optional str value used to update the character's name
+    :param roll_value: Optional int value used to update the character's roll value
+    :param modifier: Optional integer value used to update a character's roll modifier
+    :return: True if the data was updated and False if no change was made
+    """
+    #Formats the modified data for the SQLite update query
+    updated_data: str = ""
+    if not (char_name == None):
+        updated_data = updated_data + f"{NAME} = '{char_name}'"
+    if not (roll_value == None):
+        if not (updated_data == ""):
+            updated_data = updated_data + ", "
+        updated_data = updated_data + f"{ROLL} = {roll_value}"
+    if not (modifier == None):
+        if not (updated_data == ""):
+            updated_data = updated_data + ", "
+        updated_data = updated_data + f"{MODIFIER} = {modifier}"
+
+    #Executes SQLite UPDATE only if data was changed
+    if not (updated_data == ""):
+        conn = sqlite3.Connection(DATABASE_DIRECTORY)
+        conn.execute("PRAGMA foreign_keys = ON;")
+        cursor = conn.cursor()
+
+        cursor.execute(f"""
+            UPDATE {ORDER_COMMAND_TABLE}
+            SET {updated_data}
+            WHERE rowid = {char_id};""")
+        
+        conn.commit()
+        conn.close()
+
+        return True
+    else:
+        return False
+
+def get_character_by_id(char_id: int):
+    """
+    Returns the data for a particular character corresponding to a specified rowid within the order command table.
+
+    :param char_id: rowid corresponding to a target character
+    :return: None if the row does not exist or a tuple if the row does exist. Tuple indices: [0]: character name, [1]: roll value, [2]: modifier
+    """
+    conn = sqlite3.Connection(DATABASE_DIRECTORY)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+        SELECT {NAME}, {ROLL}, {MODIFIER}
+        FROM {ORDER_COMMAND_TABLE}
+        WHERE rowid = {char_id};""")
+    
+    selection = cursor.fetchone()
+
+    conn.close()
+
+    print(type(selection))
+    return selection
